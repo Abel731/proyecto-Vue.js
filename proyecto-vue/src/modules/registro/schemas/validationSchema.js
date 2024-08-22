8fa1ec6f82e145e96aea5386f94c1018b8b13fa1
Abel731 <abelp9289@gmail.com>
2024-08-22 15:38:15 -0400

    retoques estéticos al formulario en validationSchema

--- proyecto-vue/src/modules/registro/schemas/validationSchema.js
+++ proyecto-vue/src/modules/registro/schemas/validationSchema.js
@@ -1,11 +1,11 @@
 import * as yup from 'yup';
 
 export const schema = yup.object().shape({
-    nombre: yup.string().required(),
+    nombre: yup.string().required('debe de completar este campo'),
     //aqui utlice expresiones regulares para validar en este caso
     //que tenga al menos un numero en la direccion 
-    direccion: yup.string().matches(/\d/, 'La dirección debe contener al menos un número').required(),
+    direccion: yup.string().matches(/\d/, 'La dirección debe contener elnúmero de la residencia').required('debe de completar este campo'),
     //aqui que solo tenga digitos :)
-    telefono: yup.string().matches(/^\d+$/, 'solo numeros').required('El número de teléfono es obligatorio'),
-    correo: yup.string().email().required()
+    telefono: yup.string().matches(/^\d+$/, 'sólo puede ingresar numeros').required('El número de teléfono es obligatorio'),
+    correo: yup.string().email('el @ esta mal ubicado').required('debe de completar este campo')
 });
